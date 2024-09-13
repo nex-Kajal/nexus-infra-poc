cf3f7a08eba7647fc98ea15ee9e50b76934aa308
krohthaan-coforge <kajal.rohthaan@coforge.com>
2024-09-13 11:45:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 const App = () => {
   const {t} = useTranslation();
-  const {line1, line2} = t("description", {channel: "NEXUS-COFORGE....!!!!"});
+  const {line1, line2} = t("description", {channel: "Hey..!! NEXUS-COFORGE....!!!!"});
 
   return (
     <div className="container">
